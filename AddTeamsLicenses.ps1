c73eafea45eb8dd847d4c7375a1208f493a60b6d
linuxx <linuxx@users.noreply.github.com>
2024-12-26 15:10:35 -0500

    Update AddTeamsLicenses.ps1
    Fixed remove license and change verbiage on the variables

--- AddTeamsLicenses.ps1
+++ AddTeamsLicenses.ps1
@@ -12,7 +12,7 @@ $LimitOne = $false # Set to $true to only process one user (for testing purposes
 
 # Define SkuIds
 $noTeamsSkuId = "dcf0408c-aaec-446c-afd4-43e3683943ea" # Microsoft 365 E3 (no Teams)
-$fullTeamsSkuId = "05e9a617-0261-4cee-bb44-138d3ef5d965" # Microsoft 365 E3
+$e3WithTeamsSkuId = "05e9a617-0261-4cee-bb44-138d3ef5d965" # Microsoft 365 E3
 $teamsEnterpriseSkuId = "7e31c0d9-9551-471d-836f-32ee72be4a01" # Microsoft Teams Enterprise New
 
 # Fetch users from Azure AD with the desired filters
@@ -26,12 +26,12 @@ $users = if ($LimitOne) {
 
 foreach ($user in $users) {
     # Check license assignments for the user
-    $hasFullTeams = $user.AssignedLicenses | Where-Object { $_.SkuId -eq $fullTeamsSkuId }
+    $hasE3withTeams = $user.AssignedLicenses | Where-Object { $_.SkuId -eq $e3WithTeamsSkuId }
     $hasNoTeams = $user.AssignedLicenses | Where-Object { $_.SkuId -eq $noTeamsSkuId }
     $hasTeamsEnterprise = $user.AssignedLicenses | Where-Object { $_.SkuId -eq $teamsEnterpriseSkuId }
 
-    # Perform actions only if the user has "Full Teams" license
-    if ($hasFullTeams) {
+    # Perform actions only if the user has "e3 with teams" license
+    if ($hasE3withTeams) {
         Write-Host "Processing user: $($user.DisplayName) ($($user.UserPrincipalName))"
 
         if ($TestMode) {
@@ -41,7 +41,7 @@ foreach ($user in $users) {
                 # Perform license swap
                 Set-MgUserLicense -UserId $user.UserPrincipalName \
                     -AddLicenses @(@{ SkuId = $noTeamsSkuId }, @{ SkuId = $teamsEnterpriseSkuId }) \
-                    -RemoveLicenses $fullTeamsSkuId
+                    -RemoveLicenses @($fullTeamsSkuId)
                 Write-Host "Successfully swapped licenses for $($user.DisplayName)"
             } catch {
                 Write-Warning "Failed to swap licenses for $($user.DisplayName): $_"
